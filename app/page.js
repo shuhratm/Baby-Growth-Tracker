b2c29361ca89fd7cd1a0e61fc4f1c061e97787fa
shuhratm <shuhratm@gmail.com>
2025-07-17 05:16:24 -0400

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,11 @@
+'use client';
+
+import BabyGrowthTracker from './BabyGrowthTracker';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50 py-8">
+      <BabyGrowthTracker />
+    </main>
+  );
+}
